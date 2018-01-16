de52e2760ea9812c664b3c449ac760c3321b3ec6
Harvey Zack <laozhaochaguan@gmail.com>
2018-01-16 14:18:42 +0800

    update

--- meta.js
+++ meta.js
@@ -2,6 +2,19 @@ module.exports = {
   before: (metalsmith, opts, helpers) => {
     console.log('before');
   },
+  helpers: {
+    if_or(v1, v2, options) {
+
+      if (v1 || v2) {
+        return options.fn(this)
+      }
+
+      return options.inverse(this)
+    },
+    template_version() {
+      return templateVersion
+    },
+  },
   prompts: {
     name: {
       when: 'isNotTest',
